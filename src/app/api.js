1d51eb95ee67f4362953186628ec7a4f665cf595
MuhammadZeb1 <zebafridiuu@gmail.com>
2026-02-14 14:08:55 +0500

    replace the link

--- src/app/api.js
+++ src/app/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: "http://localhost:5000", // replace with your backend base URL
+  // baseURL: "http://localhost:5000", // replace with your backend base URL
+  baseURL: "https://tailor-app-backend-eight.vercel.app", // replace with your backend base URL
   headers: {
     "Content-Type": "application/json",
   },
